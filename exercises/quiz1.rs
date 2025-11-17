b82fe79f0e7a7e52cbc5479b0379d9542fa11994
cyz-ing <1316769374@qq.com>
2025-11-17 05:54:22 +0000

    finish quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -20,7 +20,7 @@ fn calculate_price_of_apples(number: i32) -> i32 {
     if number > 40 {
         number
     } else {
-        number * 2
+        number * 3
     }
 }
 
@@ -32,8 +32,8 @@ fn verify_test() {
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
 
-    assert_eq!(70, price1);
-    assert_eq!(80, price2);
+    assert_eq!(105, price1);
+    assert_eq!(120, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
 }
